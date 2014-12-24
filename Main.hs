0b858db83648ba87e07ef34d94a2dc04f90fb2ba
Peter Duchovni <peter.duchovni@mssm.edu>
2014-12-24 16:15:42 -0500

    Fudget example and makefile

--- Main.hs
+++ Main.hs
@@ -1,26 +1,38 @@
-{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
-
 module Main where
 
-import Control.Applicative
-import Control.Monad
-import Data.Maybe
-import Data.IORef
-import Data.List
-import Data.IxSet
-import qualified Data.Map as Map
-import Data.Map (Map)
-import qualified	Graphics.UI.Threepenny 			as UI
-import				Graphics.UI.Threepenny.Core
-import qualified CCGServer.Tagging as Tag
-import qualified CCGServer.Filtering as Filt
+import Fudgets
 
-setup = fst (Filt.setup,Tag.setup)
+main = fudlogue (shellF "Pocket Calculator" calcF)
 
-main :: IO ()
-main = do
-	startGUI defaultConfig
-		{ tpPort	= Just 10000
-		, tpStatic	= Just "../wwwroot"
-		} setup
+calcF = intDispF >==< mapstateF calc [0] >==< buttonsF
+
+data Buttons = Plus | Minus | Times | Div | Enter | Digit Int deriving (Eq)
+
+buttonsF = placerF (matrixP 4) (
+                listF [ d 7, d 8, d 9, op Div
+                      , d 4, d 5, d 6, op Times
+                      , d 1, d 2, d 3, op Minus
+                      , hole, d 0, ent, op Plus
+                      ])
+    where
+        d n = (Digit n, buttonF (show n))
+        ent = op Enter
+        hole = (Enter, holeF)
+        op o = (o, buttonF (opLabel o))
+            where
+                opLabel Plus = "+"
+                opLabel Minus = "-"
+                opLabel Times = "*"
+                opLabel Div = "/"
+                opLabel Enter = "Ent"
+
+calc (n:s)  (Digit d, _) = new (n*10+d) s
+calc s      (Enter,_)    = (0:s, [])
+calc (y:x:s)(Plus,_)     = new (x+y) s
+calc (y:x:s)(Minus,_)    = new (x-y) s
+calc (y:x:s)(Times,_)    = new (x*y) s
+calc (y:x:s)(Div,_)      = new (x`div`y) s
+calc s      _            = (s,[])
+
+new n s = (n:s, [n])
 
--- make
+++ make
@@ -4,10 +4,10 @@ arg=${1:-"main"}
 
 case "$arg" in
 	main) 
-		ghc --make Main.hs -o ccgserver
+		ghc --make Main.hs -o fuget -package fudgets
 		;;
 	clean)
-		rm -rf *.o *.hi */*.o */*.hi ccgserver
+		rm -rf *.o *.hi */*.o */*.hi fuget
 		;;
 	unpack)
 		tar xzf jpgs.tgz
