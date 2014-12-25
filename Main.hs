5319293b9accdbeaea249ed7101ebcb394acef94
Peter Duchovni <peter.duchovni@mssm.edu>
2014-12-24 23:32:28 -0500

    Barebones happstack code
    Implemented barebones happstack code in Main.hs

--- Main.hs
+++ Main.hs
@@ -1,26 +1,10 @@
-{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
-
 module Main where
 
-import Control.Applicative
+import Happstack.Server (nullConf, simpleHTTP, toResponse, ok, dir, seeOther, path)
 import Control.Monad
-import Data.Maybe
-import Data.IORef
-import Data.List
-import Data.IxSet
-import qualified Data.Map as Map
-import Data.Map (Map)
-import qualified	Graphics.UI.Threepenny 			as UI
-import				Graphics.UI.Threepenny.Core
-import qualified CCGServer.Tagging as Tag
-import qualified CCGServer.Filtering as Filt
-
-setup = fst (Filt.setup,Tag.setup)
 
 main :: IO ()
-main = do
-	startGUI defaultConfig
-		{ tpPort	= Just 10000
-		, tpStatic	= Just "../wwwroot"
-		} setup
-
+main = simpleHTTP nullConf $ msum
+    [ dir "hello" $ path $ \s -> ok $ "Hello, " ++ s
+    , seeOther "/hello/World" "/hello/World"
+    ]
