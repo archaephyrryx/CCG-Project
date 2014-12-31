9dc83b4e21a97a0c83fead06aae93e6972f3722b
Peter Duchovni <peter.duchovni@mssm.edu>
2014-12-31 13:53:46 -0500

    Added blaze-html templates

--- Main.hs
+++ Main.hs
@@ -1,10 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Happstack.Server (nullConf, simpleHTTP, toResponse, ok, dir, seeOther, path)
+import Happstack.Server
 import Control.Monad
+import Text.Blaze ((!))
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+
+appTemplate :: String -> [H.Html] -> H.Html -> H.Html
+appTemplate title headers body = 
+    H.html $ do
+        H.head $ do
+            H.title (H.toHtml title)
+            H.meta ! A.httpEquiv "Content-Type"
+                   ! A.content "text/html;charset=utf-8"
+            sequence_ headers
+        H.body $ do
+            body
+
+
+helloBlaze :: ServerPart Response
+helloBlaze =
+    ok $ toResponse $ 
+        appTemplate "Hello, Blaze!"
+                    [H.meta ! A.name "keywords"
+                            ! A.content "happstack, blaze, html"
+                    ]
+                    (H.p $ do "Hello, "
+                              H.b "blaze-html!"
+                              H.br
+                              )
 
 main :: IO ()
-main = simpleHTTP nullConf $ msum
+main = simpleHTTP nullConf $ helloBlaze
+
+{-
+msum
     [ dir "hello" $ path $ \s -> ok $ "Hello, " ++ s
     , seeOther "/hello/World" "/hello/World"
     ]
+-}
