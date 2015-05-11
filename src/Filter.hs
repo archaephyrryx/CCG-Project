f3625045b26a2a8210c9d1c61df8d0c7e3bec741
Peter Duchovni <peter.duchovni@mssm.edu>
2015-05-11 16:14:10 -0400

    Unified Filter.hs

--- src/Filter.hs
+++ src/Filter.hs
@@ -2,47 +2,103 @@
     DeriveDataTypeable, GeneralizedNewtypeDeriving,
 	RecordWildCards, TemplateHaskell, TypeFamilies,
 	OverloadedStrings #-}
+module Filter ( Filter
+              , cardHandler
+              , deckHandler
+        ) where
 
-module Filter where
-
-import Control.Applicative
-import Control.Monad
-import Data.Maybe
-import Data.IORef
-import Data.List
-import Data.IxSet
-import Data.Data (Data, Typeable)
-import qualified Data.Map as Map
+---------------------------------------------
 import qualified Data.Text as T
-import Data.Map (Map)
 import Text.Digestive
 import Text.Digestive.Heist
 import Text.Digestive.Snap
 import           Snap.Core (writeText)
+import           Control.Applicative
+import           Snap.Core
 import           Snap.Snaplet
-import           Snap.Snaplet.Heist (heistLocal, render)
 import           Application
+import           Snap.Snaplet.Heist
+import           Data.Maybe
+import           Control.Monad
+import           Heist
+import qualified Heist.Interpreted as I
 ---------------------------------------------
+import           Application
+import           CCG hiding (render)
 
-data Query = Query { color :: Color } deriving (Show)
 
-isNotEmpty :: T.Text -> Bool
-isNotEmpty = not . T.null
+data Filter = CardFilter { colorFilter :: Color
+                         , setFilter :: CSet
+                         , typeFilter :: CardType
+                         , rarityFilter :: Rarity
+                         , powmin :: Int
+                         , powmax :: Int
+                         , costmin :: Int
+                         , costmax :: Int
+                         , reqmin :: Int
+                         , reqmax :: Int
+            }
+            | DeckFilter { colorFilter :: Color
+                         , setFilter :: CSet
+                         , typeFilter :: CardType
+                         , rarityFilter :: Rarity
+            }
+            deriving (Show)
 
---newtype Type = Type !String
-type Color = T.Text
---newtype Name = Name !String
+handleCard :: AppHandler
+handleCard = heistLocal (I.bindSplices noSplices) $ render "card"
 
-colErrMsg :: T.Text
-colErrMsg = "Color cannot be empty"
+handleDeck :: AppHandler
+handleDeck = heistLocal (I.bindSplices noSplices) $ render "deck"
 
-queryForm :: (Monad m) => Form T.Text m Query
-queryForm = Query
-    <$> "color" .: check colErrMsg isNotEmpty (text Nothing)
+selectForm :: (Monad m) => Form T.Text m Selection
+selectForm = Selection
+    <$> "colorFilter" .: choice (map (\x -> (x, (T.pack $ show x))) (Wild:spect)) Nothing
+{-    <*> "setFilter" .: choice (map (\x -> let y = toEnum x in (y, (T.pack $ show y))) [0..4]) Nothing
+    <*> "typeFilter" .: choice (map (\x -> let y = toEnum x in (y, (T.pack $ show y))) [0..5]) Nothing
+    <*> "rarityFilter" .: choice (map (\x -> let y = toEnum x in (y, (T.pack $ show y))) [0..5]) Nothing
+-}
 
-queryFormHandler :: Handler App App ()
-queryFormHandler = do
-  (view, result) <- runForm "query" queryForm
-  case result of
-    Just x  -> writeText $ T.pack $ show x
-    Nothing -> heistLocal (bindDigestiveSplices view) $ render "queryForm"
+{-
+minmaxForm :: (Monad m) => Form T.Text m MinMax
+minmaxForm = check minmaxErrMsg validRange $ MinMax
+    <$> "min" .: stringRead "Not a number" (Just  0)
+    <*> "max" .: stringRead "Not a number" (Just 99)
+    where
+        minmaxErrMsg :: T.Text
+        minmaxErrMsg = "invalid: min>max"
+        validRange :: MinMax -> Bool
+        validRange mm = (max mm) >= (min mm)
+-}
+        
+cardForm :: (Monad m) => Form T.Text m Filter
+cardForm = CardFilter
+    <$> "select" .: selectForm
+{-    <*> "powRange" .: minmaxForm
+    <*> "costRange" .: minmaxForm
+    <*> "reqRange" .: minmaxForm
+-}
+
+deckForm :: (Monad m) => Form T.Text m Filter
+deckForm = DeckFilter
+    <$> "select" .: selectForm
+
+cardHandler :: Handler App App ()
+cardHandler = do
+    (view, result) <- runForm "cardform" cardForm
+    case result of
+        Just x  -> heistLocal (bindCard x) $ render "card"
+        Nothing -> heistLocal (bindDigestiveSplices view) $ render "card-form"
+    where
+        bindCard card = 
+            I.bindSplice "card" (I.textSplice (T.pack $ show card))
+
+deckHandler :: Handler App App ()
+deckHandler = do
+    (view, result) <- runForm "deckform" deckForm
+    case result of
+        Just x  -> heistLocal (bindDeck x) $ render "deck"
+        Nothing -> heistLocal (bindDigestiveSplices view) $ render "deck-form"
+    where
+        bindDeck deck = 
+            I.bindSplice "deck" (I.textSplice (T.pack $ show deck))
