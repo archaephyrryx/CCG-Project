f2a53ca33de6f6941c97a6254911fd401eda8414
Peter Duchovni <peter.duchovni@mssm.edu>
2015-05-11 13:52:06 -0400

    Brought over Renderer 3p helper from hsp
    Mirrored addition of Renderer-general App/Core/Helper.hs module on hsp
    branch

---
+++ App/Core/Helper.hs
@@ -0,0 +1,21 @@
+module App.Core.Helper where
+
+import Renderer.Core
+import Util
+
+glue :: Rendered
+glue = string " "
+
+settext = set text
+
+hlink :: String -> String -> Rendered
+hlink url str = a # set href url # settext str
+
+noop :: Rendered
+noop = a
+
+estring :: Builder -> String -> Rendered
+estring el = (el #$).string
+
+bstring = estring b
+istring = estring i
