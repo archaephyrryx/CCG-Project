c6b74351ba59554cd522a7ec49de5650cdd4e5d8
Peter Duchovni <peter.duchovni@mssm.edu>
2015-06-05 11:54:52 +1000

    Updated Util/List, query URI w/ Filter
    Added new functions to Util.List (copied from indigestion branch)
    Start of migration away from multiparam card handler to filter-based
    unary function

--- Main.hs
+++ Main.hs
@@ -21,25 +21,30 @@ handlebar :: ServerPartT IO Response
 handlebar = page $ do
     decodeBody (defaultBodyPolicy "/tmp" 0 10000 10000)
     msum [ dir "card" $ msum
-            [ nullDir >> do mMinPower <- (readH <$>) <$> (optional $ look "minPower")
-                            mMaxPower <- (readH <$>) <$> (optional $ look "maxPower")
-                            mMinCost  <- (readH <$>) <$> (optional $ look "minCost")
-                            mMaxCost  <- (readH <$>) <$> (optional $ look "maxCost")
-                            mMinReq   <- (readH <$>) <$> (optional $ look "minReq")
-                            mMaxReq   <- (readH <$>) <$> (optional $ look "maxReq")
-                            mcolors   <- (map readC <$>) <$> (optional $ looks "color")
-                            msets     <- (map readCS <$>) <$> (optional $ looks "set")
-                            mrars     <- (map long <$>) <$> (optional $ looks "rarity")
-                            mtypes    <- (map readT <$>) <$> (optional $ looks "type")
-                            card ([mMinPower,mMaxPower],[mMinCost,mMaxCost],[mMinReq,mMaxReq]) mcolors msets mrars mtypes
+            [ nullDir >> do cFilter <- parseCardFilter
+            card ([mMinPower,mMaxPower],[mMinCost,mMaxCost],[mMinReq,mMaxReq]) mcolors msets mrars mtypes
             , path $ \s -> single s
             ]
          , dir "deck" $
-              nullDir >> do mcolors <- (map readC <$>) <$> (optional $ looks "color")
-                            msets <- (map readCS <$>) <$> (optional $ looks "set")
-                            mrars <- (map long <$>) <$> (optional $ looks "rarity")
-                            mtypes <- (map readT <$>) <$> (optional $ looks "type")
-                            deck mcolors msets mrars mtypes
+              nullDir >> do dFilter < parseDeckFilter
          , dir "home" $ home
          , home
          ]
+
+parseCardFilter = do
+    mMinPower <- (readH <$>) <$> (optional $ look "minPower")
+    mMaxPower <- (readH <$>) <$> (optional $ look "maxPower")
+    mMinCost  <- (readH <$>) <$> (optional $ look "minCost")
+    mMaxCost  <- (readH <$>) <$> (optional $ look "maxCost")
+    mMinReq   <- (readH <$>) <$> (optional $ look "minReq")
+    mMaxReq   <- (readH <$>) <$> (optional $ look "maxReq")
+    mcolors   <- (map readC <$>) <$> (optional $ looks "color")
+    msets     <- (map readCS <$>) <$> (optional $ looks "set")
+    mrars     <- (map long <$>) <$> (optional $ looks "rarity")
+    mtypes    <- (map readT <$>) <$> (optional $ looks "type")
+
+parseDeckFilter = do
+    mcolors <- (map readC <$>) <$> (optional $ looks "color")
+    msets <- (map readCS <$>) <$> (optional $ looks "set")
+    mrars <- (map long <$>) <$> (optional $ looks "rarity")
+    mtypes <- (map readT <$>) <$> (optional $ looks "type")
