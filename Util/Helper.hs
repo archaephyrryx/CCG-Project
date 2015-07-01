defcbb16f86b147361d68bb87107923e58cfc647
Peter Duchovni <peter.duchovni@mssm.edu>
2015-07-01 15:37:31 -0400

    Haddocked util, cabal from happstack
    Imported haddock-markup commented version of Util from 'Happstack'
    Imported and partially adapted Cabal config from 'Happstack'

--- Util/Helper.hs
+++ Util/Helper.hs
@@ -4,18 +4,24 @@ import Control.Monad
 import Control.Applicative
 import Util.List (one)
 
+-- |The @||@ operator lifted to applicative functors
 afor :: (Applicative f) => f Bool -> f Bool -> f Bool
 afor = liftA2 (||)
 
+-- |Ceiling-division for calculating how many bins to use
 cdiv :: Int -> Int -> Int
 x`cdiv`y | x`mod`y == 0 = x`div`y
          | otherwise = x`div`y + 1
 
+-- |A function that converts paired monads to monadic pairs
 mpair :: Monad m => (m a, m b) -> m (a,b)
 mpair (mx, my) = do { x <- mx; y <- my; return (x, y) }
 
+-- |A 'wrapped' function treats values as singleton lists
+-- > wrapped f $ x = f [x]
 wrapped :: ([a] -> b) -> (a -> b)
 wrapped = (.one)
 
+-- |Converts a normal string-parser into a basic Read(er)
 rdr :: (String -> a) -> (ReadS a)
 rdr = (wrapped (`zip`[""]) .)
--- Util/List.hs
+++ Util/List.hs
@@ -6,40 +6,76 @@ infix 9 !?
 infix 9 ??
 infixl 9 !@
 
+-- |Convenience function to test for non-emptiness
 full = not . null
 
+-- |Filters list with simultaneously traversed boolean mask
+-- prop> filtrate (map f xs) xs = filter f xs
 filtrate :: [Bool] -> [a] -> [a]
 filtrate [] _ = []
 filtrate _ [] = []
 filtrate (p:ps) (x:xs) = (p?:(x:)) $ filtrate ps xs
 
+-- |Safe fetcher-applicator that returns default value for out-of-range indices
 (!?) :: (a -> b) -> b -> Int -> ([a] -> b)
-(f!?y) n xs | n == -1 || n >= length xs = y
-            | otherwise = f (xs!!n)
+(f!?y) n xs
+  | n < 0
+  || n >= length xs
+  = y
+  | otherwise
+  = f (xs!!n)
 
+-- |Identity transformation for empty list, generated transformation
+-- otherwise (compare with '?')
 (??) :: ([a] -> (b -> b)) -> [a] -> (b -> b)
 f??[]=id
 f??x =f x
 
+-- |'for' is syntactic convenience for 'map' where the function is more
+-- complicated than the list; it is purely an argument-flipped 'map'.
 for :: [a] -> (a -> b) -> [b]
 for = flip map
 
+-- |'one' converts a value to a singleton list
 one :: a -> [a]
 one = (:[])
 
+-- |'once' wraps a function result in a singleton list
 once :: (a -> b) -> (a -> [b])
 once = (one.)
 
+-- |The 'headDef' function returns a default value for an empty list, or
+-- the head of a non-empty list
 headDef :: a -> [a] -> a
 headDef x [] = x
 headDef _ (x:_) = x
 
+
+-- |Syntactic convenience for second element of a list
+neck :: [a] -> a
 neck = (!!1)
+
+-- |Syntactic convenience for third element of a list
+body :: [a] -> a
 body = (!!2)
 
+-- |The 'mmap' function maps over maybe-lists, returning empty for
+-- Nothing or the mapped list for Just a list
+--
+-- prop> mmap f (Just xs) = map f xs
 mmap :: (a -> b) -> Maybe [a] -> [b]
 mmap = (?/).map
 
+-- | Retrieves multiple indices of a list in one pass.
+-- This works as long as the index list is sorted and contains no
+-- duplicates: the last element in the resulting list is the last
+-- element of the longest sorted, unduplicated, in-range prefix of the
+-- index list.
+--
+-- prop> _!\@[] = []
+-- prop> []!\@_ = []
+-- prop> xs!\@(one i) = (one!?[]) i xs
+-- prop> xs!\@is = xs!\@(tail.filter((==)<$>nub<*>sort).heads$is)
 (!@) :: [a] -> [Int] -> [a]
 []!@i = []
 x!@[] = []
