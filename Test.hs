4d68d645f1bd80749cf1f4fae57905b16c27e12e
Peter Duchovni <pld54@cornell.edu>
2015-12-13 21:38:09 -0500

    Links implemented
    Implemented softlinks, liquidlinks
    Added dynamic-increment counter example for ll and sl test in Test.hs

--- Test.hs
+++ Test.hs
@@ -6,6 +6,7 @@ import Graphics.UI.WX hiding (Event)
 import Reactive.Banana
 import Reactive.Banana.WX
 import Widgets.MultiSelect
+import Widgets.Links
 import Data.List
 import Tidings
 
@@ -36,13 +37,21 @@ test = do
     choicer <- multiListBox f []
     choice <- staticText f []
 
-    set f [layout := margin 10 $ row 5 $ [minsize (sz 200 300) $ widget choicer, widget choice] ]
+    counter <- staticText f []
+    inc <- button f [ text := "+0" ]
+    plus <- button f [ text := "+" ]
+
+
+    set f [layout := margin 10 $ row 5 $ [minsize (sz 200 300) $ widget choicer, widget choice, widget counter, widget inc, widget plus] ]
 
     let networkDescription :: forall t. Frameworks t => Moment t ()
         networkDescription = mdo
                 eClear <- event0 clear command
 
                 tSelections <- multiSelect choicer bAnthology bSelections bDisplay
+                
+                bPlus <- softLink plus 1
+                tInc <- liquidLink inc (pure (('+':).show)) (accumB 0 $ (+) <$> rumors bPlus)
 
                 let eSelections :: Event t [String]
                     eSelections = rumors tSelections
@@ -59,7 +68,14 @@ test = do
                     bDisplay :: Behavior t (String -> String)
                     bDisplay = pure id
 
+                    eInc :: Event t Int
+                    eInc = rumors tInc
+
+                    bCount :: Behavior t Int
+                    bCount = accumB 0 $ (+) <$> eInc
+
                 sink choice [ text :== bResults ]
+                sink counter [ text :== (show <$> bCount) ]
 
     network <- compile networkDescription
     actuate network
